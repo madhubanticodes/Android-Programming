749ad4481f3b489e3430c2d305c22dc02929f529
madhubanticodes <69532745+madhubanticodes@users.noreply.github.com>
2023-12-21 14:23:56 +0530

    Create WelcomeActivity.java

---
+++ Login/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private TextView txtWelcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        String strUsername=getIntent().getStringExtra("Username");
+        txtWelcome=findViewById(R.id.txt_welcome);
+        txtWelcome.setText("Welcome "+strUsername);
+    }
+}
