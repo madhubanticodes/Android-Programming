191d7b84847e6ff2cdd73e7817ff721c66e95a63
madhubanticodes <69532745+madhubanticodes@users.noreply.github.com>
2023-12-21 14:38:08 +0530

    Create MainActivity.java

---
+++ Two Activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.twoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText et;
+    String st;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        et=findViewById(R.id.edittext);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i=new Intent(MainActivity.this,SecondActivity.class);
+                st=et.getText().toString();
+                i.putExtra("Value",st);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
